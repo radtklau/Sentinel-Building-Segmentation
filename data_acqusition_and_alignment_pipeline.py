f8fdec3ef25da054853b9a2036d190f1397d966d
radtklau <laurids.radtke1@gmail.com>
2024-06-11 19:25:58 +0200

    finaly fixed alignment bug

--- data_acqusition_and_alignment_pipeline.py
+++ data_acqusition_and_alignment_pipeline.py
@@ -19,6 +19,7 @@ import math
 from shapely.geometry import Polygon
 import rasterio.features
 from geopy.distance import geodesic
+import geopandas as gpd
 
 def get_osm_building_data(city_name):
     if city_name not in sources.cities.available:
@@ -58,7 +59,8 @@ def get_osm_building_data(city_name):
     building_data_fp = os.path.join(building_and_sentinel_city_data_dir_name, f"{city_name}_buildings.pkl")
 
     with open(building_data_fp, 'wb') as f:
-        pickle.dump(building_geometry, f)
+        #pickle.dump(building_geometry, f)
+        pickle.dump(buildings_in_city_boundaries, f)
 
 def get_city_boundary(fp, city_name):
     osm = OSM(fp)
@@ -129,35 +131,6 @@ def plot_data(city_name, type): #plot relevant data
         plt.show()
 
 
-def buildings_pkl_to_png(city_name):
-    path_to_city_data = os.path.join("building_and_sentinel_data", city_name)
-    path_to_building_pkl = os.path.join(path_to_city_data, f"{city_name}_buildings.pkl")
-    path_to_building_png = os.path.join(path_to_city_data, f"{city_name}_buildings.png")
-    building_geometry = None
-    try:
-        with open(path_to_building_pkl, 'rb') as f:
-            building_geometry = pickle.load(f)
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-    fig, ax = plt.subplots()
-
-    # Set the figure background to transparent
-    #fig.patch.set_alpha(0.0)
-
-    # Plot the building geometry
-    if building_geometry is not None:
-        building_geometry.plot(ax=ax)
-
-    # Set the axes background to transparent
-    #ax.patch.set_alpha(0.0)
-
-    #ax.axis('off')
-
-    # Save the plot as PNG with transparent background
-    plt.savefig(path_to_building_png, transparent=True)
-    plt.close(fig)
-
 def find_extreme_points():
     boundary_coords = np.array(city_boundary["geometry"].exterior.coords)
 
@@ -193,19 +166,35 @@ def get_sentinel_image_data(temporal_extent, bands, city_name):
     max_longitude_point = city_boundary["geometry"].bounds[2]
     max_latitude_point = city_boundary["geometry"].bounds[3]
     """
-    global spatial_extent
+    #global spatial_extent
     spatial_extent = {"west":min_longitude_point[0], "south":min_latitude_point[1], \
                       "east":max_longitude_point[0], "north":max_latitude_point[1]} 
     
+    print(spatial_extent)
+
     datacube = connection.load_collection("SENTINEL2_L2A", \
             spatial_extent=spatial_extent, \
             temporal_extent=temporal_extent, \
             bands=bands
-            )
+            ) #BUG doesnt precisely deliver image in spatial_extent corners! compare Flensburg rgb top right corner with geo coords of corner on google maps!
+    
+    #job = datacube.export(filename=f"{city_name}_sentinel.tiff", format="GTiff")
+    #job.start_and_wait().download_results()
+
+    
+    building_and_sentinel_data_dir_name = "building_and_sentinel_data"
+    if not os.path.exists(building_and_sentinel_data_dir_name):
+        os.makedirs(building_and_sentinel_data_dir_name)
+
+    building_and_sentinel_city_data_dir_name = \
+        f"{building_and_sentinel_data_dir_name}/{city_name}"
+    if not os.path.exists(building_and_sentinel_city_data_dir_name):
+        os.makedirs(building_and_sentinel_city_data_dir_name)
     
     print("Downloading image data...")
-    result = datacube.download()
+    result = datacube.download(os.path.join(building_and_sentinel_city_data_dir_name, f"{city_name}.tiff"))
 
+    """
     print("Processing image data...")
     image_array = imageio.imread(result)
     clipped_image = np.clip(image_array, 0, 2500)
@@ -231,18 +220,7 @@ def get_sentinel_image_data(temporal_extent, bands, city_name):
     global im_shape
     im_shape = r_im.shape
 
-
-
-
-    print("Writing to disk...")
-    building_and_sentinel_data_dir_name = "building_and_sentinel_data"
-    if not os.path.exists(building_and_sentinel_data_dir_name):
-        os.makedirs(building_and_sentinel_data_dir_name)
-
-    building_and_sentinel_city_data_dir_name = \
-        f"{building_and_sentinel_data_dir_name}/{city_name}"
-    if not os.path.exists(building_and_sentinel_city_data_dir_name):
-        os.makedirs(building_and_sentinel_city_data_dir_name)
+    print("Writing image data to disk...")
     
     satellite_data_rgb_fp = os.path.join(building_and_sentinel_city_data_dir_name, f"{city_name}_rgb.png")
     satellite_data_r_fp = os.path.join(building_and_sentinel_city_data_dir_name, f"{city_name}_r.png")
@@ -257,7 +235,7 @@ def get_sentinel_image_data(temporal_extent, bands, city_name):
     plt.imsave(satellite_data_b_fp, b_im, cmap='gray')
     plt.imsave(satellite_data_irb_fp, irb_im)
     plt.imsave(satellite_data_vnir_fp, vnir_im, cmap='gray')
-
+    """
 def geo_coord_reprojection(city_name, polygon=True, im_corners=None):
     path_to_city_data = os.path.join("building_and_sentinel_data", city_name)
     path_to_building_data = os.path.join(path_to_city_data, f"{city_name}_buildings.pkl")
@@ -279,9 +257,9 @@ def geo_coord_reprojection(city_name, polygon=True, im_corners=None):
     utm_zone = math.floor((half_lon + 180) / 6) + 1
 
     if half_lat > 0:
-        utm_crs = f'EPSG:326{utm_zone}'
+        utm_crs = f'326{utm_zone}'
     else:
-        utm_crs = f'EPSG:327{utm_zone}'
+        utm_crs = f'327{utm_zone}'
 
     geographic_crs = 'EPSG:4326'
     #utm_crs = 'EPSG:25832'
@@ -290,6 +268,9 @@ def geo_coord_reprojection(city_name, polygon=True, im_corners=None):
     transformer = Transformer.from_crs(geographic_crs, utm_crs, always_xy=True)
 
     if polygon:
+        geo_df = gpd.GeoDataFrame(geometry=building_geometry, crs=4326)
+        utm_geo_df = geo_df.to_crs(utm_crs)
+        """
         utm_coords = []
         for polygon in building_geometry.values.data:
             building_coords = np.array(polygon.exterior.coords)
@@ -300,8 +281,8 @@ def geo_coord_reprojection(city_name, polygon=True, im_corners=None):
                 polygon_utm_coords.append(utm_coord)
 
             utm_coords.append(polygon_utm_coords)
-
-        return utm_coords
+        """
+        return utm_geo_df["geometry"].values.data
     
     else:
         utm_min_x, _ = transformer.transform(im_corners[0], im_corners[1])
@@ -326,12 +307,101 @@ def build_stacked_im(city_name, label_im):
     # Set the white pixels in the BW image to white in the RGB image
     stacked_im[mask] = [0, 0, 255]
 
-    path_to_stacked_image = os.path.join(path_to_city_data, f"{city_name}_stacked.png")
+    path_to_stacked_image = os.path.join(path_to_city_data, f"{city_name}_stacked_new2.png")
     plt.imsave(path_to_stacked_image, stacked_im)
+
+def extract_coordinates(geometry):
+    if geometry.geom_type == 'Polygon':
+        exterior_coords = list(geometry.exterior.coords)
+        return exterior_coords
+    elif geometry.geom_type == 'MultiPolygon':
+        # Handle MultiPolygon by iterating over each polygon
+        all_coords = []
+        for polygon in geometry:
+            all_coords.extend(list(polygon.exterior.coords))
+        return all_coords
+    else:
+        return []
+    
+def coords_to_rowcol(coords, transformer):
+    row_col_list = []
+    for coord in coords:
+        x, y = coord
+        row, col = transformer.rowcol(x, y)
+        row_col_list.append((row, col))
+    return row_col_list
 
 
 def label_gen(city_name):
-    utm_polygon_coords = geo_coord_reprojection(city_name)
+    path_to_city_data = os.path.join("building_and_sentinel_data", city_name)
+    path_to_building_data = os.path.join(path_to_city_data, f"{city_name}_buildings.pkl")
+    path_to_tiff_im = os.path.join(path_to_city_data, f"{city_name}.tiff")
+    tiff_img = rasterio.open(path_to_tiff_im)
+
+    print(tiff_img.crs)
+    print(tiff_img.transform)
+
+    path_to_city_data = os.path.join("building_and_sentinel_data", city_name)
+    path_to_building_data = os.path.join(path_to_city_data, f"{city_name}_buildings.pkl")
+    buildings_in_city_boundaries = None
+    try:
+        with open(path_to_building_data, 'rb') as f:
+            buildings_in_city_boundaries = pickle.load(f)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    utm_buildings = buildings_in_city_boundaries.to_crs(tiff_img.crs)
+    transformer = rasterio.transform.AffineTransformer(tiff_img.transform)
+
+    utm_buildings['coords'] = utm_buildings['geometry'].apply(extract_coordinates)
+
+    utm_buildings['row_col'] = utm_buildings['coords'].apply(lambda coords: coords_to_rowcol(coords, transformer))
+
+    path_to_city_data = os.path.join("building_and_sentinel_data", city_name)
+    path_to_rgb_image = os.path.join(path_to_city_data, f"{city_name}_rgb.png")
+    rgb_im = Image.open(path_to_rgb_image).convert('RGB')  # Convert to RGB to remove the alpha channel
+    rgb_im = np.array(rgb_im)
+
+    #fig, ax = plt.subplots(figsize=(10, 10))
+
+    x = list(utm_buildings['row_col'])
+    # Plot the GeoDataFrame with customizations
+    #utm_buildings['row_col'].plot(ax=ax)
+    polygons = [Polygon(coords) for coords in utm_buildings['row_col']]
+    pixel_polygons = []
+    for building in x:
+        pixel_polygon = []
+        for coord in building:
+            x = coord[0]
+            y = coord[1]
+            pixel_coord = (y,x)
+            #print(pixel_coord)
+            pixel_polygon.append(pixel_coord)
+        pixel_polygons.append(Polygon(pixel_polygon))
+
+    label_im = rasterio.features.rasterize(pixel_polygons, out_shape=rgb_im.shape[:2], all_touched=True)
+    #label_im = np.rot90(label_im, k=1)
+    #label_im = np.fliplr(label_im)
+    build_stacked_im(city_name, label_im)
+    """
+    label_im = np.zeros(rgb_im.shape[:2])
+     
+    for building in x:
+        for coord in building:
+            label_im[coord] = 1
+
+    build_stacked_im(city_name, label_im)
+    #build_stacked_im(city_name, label_im)
+    #path_to_stacked_image = os.path.join(path_to_city_data, f"{city_name}_stacked_new3.png")
+    #plt.imsave(path_to_stacked_image, rgb_im)
+    """
+    sys.exit()
+
+    
+
+    #utm_polygon_coords = geo_coord_reprojection(city_name)
+
+
 
     global city_boundary
     min_longitude_point, min_latitude_point, max_longitude_point, max_latitude_point = find_extreme_points()
@@ -344,7 +414,7 @@ def label_gen(city_name):
     pixel_polygons = []
     for utm_polygon_coord in utm_polygon_coords:
         pixel_polygon = []
-        for utm_coord in utm_polygon_coord:
+        for utm_coord in np.array(utm_polygon_coord.exterior.coords):
             utm_x_coord = utm_coord[0]
             utm_y_coord = utm_coord[1]
             x_dist_im_corner = utm_x_coord - utm_im_corners[0]
@@ -363,7 +433,7 @@ def label_gen(city_name):
     path_to_rgb_image = os.path.join(path_to_city_data, f"{city_name}_r.png")
     r_im = Image.open(path_to_rgb_image)
     r_im = np.array(r_im)      
-    global im_shape
+    #global im_shape
     im_shape = r_im.shape[:2]  
     #REMOVE#
 
@@ -388,6 +458,6 @@ def a_1_pipeline(city_name):
     get_sentinel_image_data(temporal_extent, bands, city_name)
     label_gen(city_name)
 
-city_name = "Flensburg"
+city_name = "Hamm"
 a_1_pipeline(city_name)
 #plot_data("Hamm", "buildings")
