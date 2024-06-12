dfa5e33b4a281a7f870336d17bd99dc9eea89b92
radtklau <laurids.radtke1@gmail.com>
2024-06-12 20:51:29 +0200

    fixed bugs but to slow

--- data_preparation_pipeline.py
+++ data_preparation_pipeline.py
@@ -3,9 +3,12 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 import sys
+from tqdm import tqdm
 
 def prepare_tensors(city_names, patch_size=64):
-    for city_name in city_names:
+    label_tensors = []
+    feature_tensors = []
+    for city_name in tqdm(city_names):
         path_to_city_data = os.path.join("building_and_sentinel_data", city_name)
         path_to_rgb_image = os.path.join(path_to_city_data, f"{city_name}_rgb.png")
         path_to_label_image = os.path.join(path_to_city_data, f"{city_name}_buildings.png")
@@ -19,36 +22,53 @@ def prepare_tensors(city_names, patch_size=64):
         label_matrix[building_mask] = 1
 
         if patch_size > label_matrix.shape[0] or patch_size > label_matrix.shape[1]:
-            print("ERROR. Patch is bigger than image.")
+            print("ERROR. Patch size is bigger than image.")
             sys.exit()
         
-        label_tensor = np.zeros((patch_size, patch_size))
-        feature_tensor = np.zeros((patch_size, patch_size, 3))
+        label_tensor = np.zeros((1, patch_size, patch_size))
+        feature_tensor = np.zeros((1, patch_size, patch_size, 3))
 
-        min_dim_len = min(label_im.shape)
+        min_dim_len = min(label_matrix.shape)
         indices = np.arange(0, min_dim_len - patch_size, patch_size)
 
         for i in range(len(indices)):
+            print(i)
             for index in indices:
                 label_patch = label_matrix[index:index+patch_size, indices[i]:indices[i]+patch_size]
                 feature_patch = rgb_im[index:index+patch_size, indices[i]:indices[i]+patch_size,:]
 
-                label_tensor = label_tensor.stack((label_tensor, label_patch), axis=0)
-                feature_tensor = feature_tensor.stack((feature_tensor, feature_patch), axis=0)
+                label_patch = np.expand_dims(label_patch, axis=0)
+                feature_patch = np.expand_dims(feature_patch, axis=0)
+
+                label_tensor = np.concatenate((label_tensor, label_patch), axis=0)
+                feature_tensor = np.concatenate((feature_tensor, feature_patch), axis=0)
 
         label_tensor = label_tensor[1:]
         feature_tensor = feature_tensor[1:]
 
-        
+        label_tensor, feature_tensor = remove_cloudy_patches(label_tensor, feature_tensor)
+
+        label_tensors.append(label_tensor)
+        feature_tensors.append(feature_tensor)
+
+    return label_tensors, feature_tensors
+
+def build_final_tensors(label_tensors, feature_tensors):
+    #stack into one tensor for labels and one for features
+    #split into train, test and val dataset based on some rules (equal feature dist etc)
+    #store train, test and val datasets in own dir with metadata about preparation params
 
 
-
-def tensorize(image, labels):
     pass
+
+def remove_cloudy_patches(label_tensor, feature_tensor):
+    return label_tensor, feature_tensor
+
 
 def a_2_pipeline(city_names):
 
-    prepare_tensors(city_names)
+    label_tensors, feature_tensors = prepare_tensors(city_names)
+    build_final_tensors(label_tensors, feature_tensors) 
     pass
 
 city_names = ["Berlin", "Denver", "Wien", "Helsinki", "Hamm"]
